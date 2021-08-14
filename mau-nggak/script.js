2c99b94829c51e0b1d2aab9a5aa7420c86aa701c
Alvito Devanova <70482031+alvitodev@users.noreply.github.com>
2021-08-14 20:01:02 +0700

    Create script.js

---
+++ mau-nggak/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  alert("I LOVE YOUU ❤️❤️ Bagi kontaknya dong... :)");
+}
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+  var changeTop = (Math.random() * ($(window).height() - $("button").height()));
+  var changeLeft = (Math.random() * ($(window).width() - $("button").width()));
+  $("button").css("margin-top", changeTop + "px");
+  $("button").css("margin-left", changeLeft + "px");
+});
