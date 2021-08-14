3bb111942e5e90b01a8aa774a58a8626d4f3e5c6
Alvito Devanova <70482031+alvitodev@users.noreply.github.com>
2021-08-14 20:18:50 +0700

    Update script.js

--- mau-nggak/script.js
+++ mau-nggak/script.js
@@ -5,8 +5,8 @@ function myFunction() {
 var button = document.querySelector("button");
 
 button.addEventListener("click", function() {
-  var changeTop = (Math.random() * ($(window).height() - $("button").height()));
-  var changeLeft = (Math.random() * ($(window).width() - $("button").width()));
+  var changeTop = (Math.random() + ($(window).height() - $("button").height()));
+  var changeLeft = (Math.random() + ($(window).width() - $("button").width()));
   $("button").css("margin-top", changeTop + "px");
   $("button").css("margin-left", changeLeft + "px");
 });
