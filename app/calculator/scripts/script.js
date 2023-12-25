52aef404d0a6f5d9a75ff37ffeba40a4c78a12de
Alvito Devanova <70482031+alvitodev@users.noreply.github.com>
2023-12-25 18:16:19 +0700

    adwadwa

--- app/calculator/scripts/script.js
+++ app/calculator/scripts/script.js
@@ -40,12 +40,7 @@ function calculate(value) {
       }
       result = num1 / num2;
       break;
-    case "%":
-      result = num1 / 100;
-      break;
-    case "**":
-      result = Math.pow(num1, num2);
-      break;
+    
     default:
       result = "Invalid operator";
   }
