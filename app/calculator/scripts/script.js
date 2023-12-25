a028d7d1e3c821116ebb755a7397850711952670
Alvito Devanova <70482031+alvitodev@users.noreply.github.com>
2023-12-25 17:57:55 +0700

    wdwddw

--- app/calculator/scripts/script.js
+++ app/calculator/scripts/script.js
@@ -8,10 +8,10 @@ const toast = document.getElementById("toast");
 
 function calculate(value) {
   // Memecah ekspresi berdasarkan operator
-  const parts = value.split(/\+|\-|\*|\//);
+  const parts = value.split( /\+ | \- | \* | \// | '%' );
 
   // Menangani kesalahan input
-  if (parts.length !== 2 || !value.includes("+") && !value.includes("-") && !value.includes("*") && !value.includes("/")) {
+  if (!value.includes("+") && !value.includes("-") && !value.includes("*") && !value.includes("/") && !value.includes("%")) {
     res.value = "Invalid Input";
     return;
   }
@@ -39,6 +39,9 @@ function calculate(value) {
         return;
       }
       result = num1 / num2;
+      break;
+    case "%":
+      result = num1 / 100;
       break;
     default:
       result = "Invalid operator";
